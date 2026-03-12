5b208b82c56fa437380fb4f24672dc828c0dc57f
elukner <elukner@gmail.com>
2026-03-12 14:12:10 -0600

    Installed and config tailwindcss

--- JobSyncAI/src/components/auth/AuthForm.tsx
+++ JobSyncAI/src/components/auth/AuthForm.tsx
@@ -1,2 +1,11 @@
 //The Login UI Component
 //file to globally track if you're logged in
+//When you are ready to type, your goal for this ticket is to build a simple form that has:
+
+// An Email input field.
+
+// A Password input field.
+
+// A Sign In button.
+
+// A Sign Up button.
