a38f9dad061a9d924f5226e802927c241a26696a
elukner <elukner@gmail.com>
2026-03-31 14:57:09 -0600

    fix: update AuthContextType to return Promises for signUp and signIn methods feat: worked on signUp and signIn methods for supabase

--- JobSyncAI/src/contexts/AuthContext.tsx
+++ JobSyncAI/src/contexts/AuthContext.tsx
@@ -5,10 +5,11 @@ import { supabase } from '../lib/supabase' // Connection to supabase
 //file for your visual login form
 
 //this is what user data will go in the bucket
-type AuthContextType={
-    session: any
-    signUp: (email:string,password:string)=>void
-    signIn: (email:string,password:string)=>void
+type AuthContextType = {
+  session: any
+  // Update these two lines to tell TS we are returning a Promise with data
+  signUp: (email: string, password: string) => Promise<{ success: boolean; data?: any; error?: any }>
+  signIn: (email: string, password: string) => Promise<{ success: boolean; data?: any; error?: any }>
 }
 
 //this is the bucket for the user data
@@ -19,12 +20,31 @@ export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
 
     const signUp = async (email: string, password: string) => {
         // You'll put the supabase.auth.signUp() magic he shows in the video here!
+        const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        })
+        if(error){
+            console.error("Error with signing up:", error)
+            return {success: false,error}
+        }
+        return {success: true,data}
 
 
     }
 
     const signIn = async (email: string, password: string) => {
         // You'll put the supabase.auth.signInWithPassword() magic here!
+        //TODO
+        const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        })
+        if(error){
+            console.error("Error with signing up:", error)
+            return {success: false,error}
+        }
+        return {success: true,data}
     }
 
     return(
