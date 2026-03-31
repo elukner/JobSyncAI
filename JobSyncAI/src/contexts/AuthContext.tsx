617044358152ad7adbed999ca56c265210ab514f
elukner <elukner@gmail.com>
2026-03-31 13:40:24 -0600

    feat: configure secure database routing and scaffold AuthContext

--- JobSyncAI/src/contexts/AuthContext.tsx
+++ JobSyncAI/src/contexts/AuthContext.tsx
@@ -1,2 +1,27 @@
+import { createContext, useContext, useState, useEffect, type ReactNode } from 'react'
+import { supabase } from '../lib/supabase' // Connection to supabase
+
 //The Auth State Manager
 //file for your visual login form
+
+//this is what user data will go in the bucket
+type AuthContextType={
+    session: any
+    signUp: (email:string,passowrd:string)=>void
+    signIn: (email:string,passowrd:string)=>void
+}
+
+//this is the bucket for the user data
+const AuthContext=createContext<AuthContextType|undefined>(undefined)
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+    const [session, setSession] = useState(undefined)
+
+    const signUp = async (email: string, password: string) => {
+        // You'll put the supabase.auth.signUp() magic he shows in the video here!
+    }
+
+    const signIn = async (email: string, password: string) => {
+        // You'll put the supabase.auth.signInWithPassword() magic here!
+    }
+}
--- JobSyncAI/src/lib/supabase.ts
+++ JobSyncAI/src/lib/supabase.ts
@@ -1,7 +1,13 @@
 //The Database Handshake File
 //file to connect to database
+import {createClient} from '@supabase/supabase-js'
 
+// Grab the secret keys from your environment variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
+// Create the connection and export it so AuthContext can use it
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 
 
