a4e72edf84c31c950ca9bc6f908d6e951239a952
elukner <elukner@gmail.com>
2026-04-01 10:21:34 -0600

    feat: implement router setup with AuthForm and Dashboard routes

---
+++ JobSyncAI/src/router.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom'
+import App from "./App"
+import Dashboard from './pages/Dashboard'
+import AuthForm from './components/auth/AuthForm'
+
+export const router = createBrowserRouter([
+    { path: "/authForm", element: <AuthForm/>},
+    { path: "/dashboard", element: <Dashboard/>},
+  //TODO  { path: "/Login", element: <App/>},
+  //TODO  { path: "/SignUP", element: <App/>},
+])
