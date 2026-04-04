dbc3cde2c4efcf3bd5e1cff3fbcf3042ba4c912c
elukner <elukner@gmail.com>
2026-04-04 13:20:35 -0600

    feat: update AuthForm component to use React Hook Form for input handling and validation

--- JobSyncAI/src/components/auth/AuthForm.tsx
+++ JobSyncAI/src/components/auth/AuthForm.tsx
@@ -58,27 +58,36 @@ const onValidSubmit = async (data: AuthFormOutput) => {
     });
 
   return (
-    <div>
+<div>
       <form onSubmit={handleSubmit(onValidSubmit)} className="max-w-md mx-auto pt-10">
         <h2 className="font-bold pb-2 text-2xl">{title}</h2>
         {footerLink}
-        <div className="mt-4">
-          <input
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            placeholder='Email'
-            className='block w-full p-2 mb-4 border rounded text-black'
-            required
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            placeholder='Password'
-            className='block w-full p-2 mb-4 border rounded text-black'
-            required
-          />
+        <div className="mt-4 flex flex-col space-y-4">
+
+          <div>
+            <input
+              type="email"
+              {...register("email")} 
+              placeholder='Email'
+              className='block w-full p-2 border rounded text-black'
+            />
+            {errors.email && (
+              <span className="text-red-500 text-xs">{errors.email.message}</span>
+            )}
+          </div>
+
+          <div>
+            <input
+              type="password"
+              {...register("password")} 
+              placeholder='Password'
+              className='block w-full p-2 border rounded text-black'
+            />
+            {errors.password && (
+              <span className="text-red-500 text-xs">{errors.password.message}</span>
+            )}
+          </div>
+
           <button
             type="submit"
             disabled={loading}
