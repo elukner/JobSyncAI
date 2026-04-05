c50c72ec7919f0892edf6ea9387425eb8a2d1bcf
elukner <elukner@gmail.com>
2026-04-05 15:18:04 -0600

    feat: implement getScoreColor function for score-based color determination

--- JobSyncAI/src/lib/utils.ts
+++ JobSyncAI/src/lib/utils.ts
@@ -6,6 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 
-export function getScoreColor(int: score) {
-  return null
+export function getScoreColor(score: number) {
+  if (score < 50) return "text-red-500";
+
+  if (score <= 79) return "text-yellow-500";
+
+  if (score >= 80) return "text-green-500";
 }
